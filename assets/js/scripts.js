7263b3eb1dd2755dd44760f4a985248380ea6408
crisdinamarca <60186023+crisdinamarca@users.noreply.github.com>
2020-01-23 16:57:09 -0300

    Avances del sitio
    Las estructuras de navbar, slider, sobre mi, mi experiencia y trabajos están listas, falta cambiar información simulada.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,24 +1,15 @@
-(function ($) {
-
-    var navbar = $('.navbar');
-    var lastScrollTop = 0;
-
-    $(window).scroll(function () {
-        var st = $(this).scrollTop();
-        // Scroll down
-        if (st > lastScrollTop) {
-            navbar.fadeOut();
-        } 
-        // Scroll up but still lower than 200 (change that to whatever suits your need)
-        else if(st < lastScrollTop && st > 200) {
-            navbar.fadeIn();
-            navbar.removeClass('navbar-light bg-transparent').addClass('navbar-dark bg-custom');
-        }
-        // Reached top
-        else {
-            navbar.removeClass('navbar-dark bg-custom').addClass('navbar-light bg-transparent');
-        }
-        lastScrollTop = st;
+$(document).ready(function(){
+    $("a").on('click', function(event) {
+      if (this.hash !== "") {
+        event.preventDefault();
+        var hash = this.hash;
+  
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
+  
+          window.location.hash = hash;
+        });
+      } // End if
     });
-
-})(jQuery);
+  });
