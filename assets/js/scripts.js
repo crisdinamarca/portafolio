717b3e34b8fa4cdf46c2491ded2771263219acde
crisdinamarca <60186023+crisdinamarca@users.noreply.github.com>
2020-01-24 14:59:24 -0300

    Avances 15 horas Viernes
    Ajustes de casi todas las secciones, falta el footer.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-    /* SWIPER */
+    /* SMOOTH SCROLL */
     $("a").on('click', function(event) {
       if (this.hash !== "") {
         event.preventDefault();
@@ -17,5 +17,30 @@ $(document).ready(function(){
 
     /* TOOLTIP */
     $('[data-toggle="tooltip"]').tooltip();
+
+    /* SWIPER */
+    var mySwiper = new Swiper ('.swiper-container', {
+      loop: true,
+        pagination: {
+          el: '.swiper-pagination',
+        },
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+        breakpoints: {
+          768: {
+            slidesPerView: 1,
+            spaceBetween: 0,
+            autoplay: {
+              delay: 5000,
+            },
+          },
+          1024: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+        }
+    })
 
   });
