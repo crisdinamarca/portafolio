35196170abf3317e62c8abea04be18360e7a9f3b
crisdinamarca <60186023+crisdinamarca@users.noreply.github.com>
2020-01-24 16:54:36 -0300

    Ajustes Finales
    Ajustes finales de sitio web portafolio

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,22 +1,9 @@
 $(document).ready(function(){
 
-    /* SMOOTH SCROLL */
-    $("a").on('click', function(event) {
-      if (this.hash !== "") {
-        event.preventDefault();
-        var hash = this.hash;
-  
-        $('html, body').animate({
-          scrollTop: $(hash).offset().top
-        }, 800, function(){
-  
-          window.location.hash = hash;
-        });
-      }
-    });
-
     /* TOOLTIP */
     $('[data-toggle="tooltip"]').tooltip();
+
+    /* LIGHTBOX */
 
     /* SWIPER */
     var mySwiper = new Swiper ('.swiper-container', {
